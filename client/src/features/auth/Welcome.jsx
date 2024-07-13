615cfa91de9a00b80398c67f599821e5f2cfe2e6
kimamovic21 <imamovic.kerim@gmail.com>
2024-07-13 12:30:20 +0200

    Chapter 07: React & Redux Forms
    -u .eslintrc.csj fajl dodana iskljucena opcija za prop-types
    -u komponentu App.jsx importovane nove kreirane komponente
    -u store.js fajl dodana funkcija setupListeners()
    -kreiran folder config i roles.js fajl
    -u folderu auth kreirana komponenta Prefetch.jsx
    -manje izmjene u komponenti Welcome.jsx
    -u folderu notes kreirane nove komponente: EditNote.jsx, EditNoteForm.jsx, NewNote.jsx, NewNoteForm.jsx
    -u notesApiSlice.js fajlu kreirane nove funkcije
    -u NotesList.jsx komponentu dodane opcije za funkciju useGetNotesQuery()
    -u folderu users kreirane nove komponente: EditUser.jsx, EditUserForm.jsx, NewUserForm.jsx
    -u usersApiSlice.js fajlu kreirane nove funkcije
    -u UsersList.jsx komponentu dodane opcije za funkciju useGetUsersQuery()
    -kreirane nove klase u index.css fajlu
    -manje izmjene u documentation.txt fajlu

--- client/src/features/auth/Welcome.jsx
+++ client/src/features/auth/Welcome.jsx
@@ -7,10 +7,19 @@ const Welcome = () => {
 
     const content = (
         <section className="welcome">
+
             <p>{today}</p>
+
             <h1>Welcome!</h1>
+
             <p><Link to="/dash/notes">View techNotes</Link></p>
+
+            <p><Link to="/dash/notes/new">Add New techNote</Link></p>
+
             <p><Link to="/dash/users">View User Settings</Link></p>
+
+            <p><Link to="/dash/users/new">Add New User</Link></p>
+
         </section>
     )
 
--- client/src/features/notes/NotesList.jsx
+++ client/src/features/notes/NotesList.jsx
@@ -8,7 +8,11 @@ const NotesList = () => {
         isSuccess,
         isError,
         error
-    } = useGetNotesQuery()
+    } = useGetNotesQuery(undefined, {
+        pollingInterval: 15000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    })
 
     let content
 
@@ -43,8 +47,8 @@ const NotesList = () => {
             </table>
         )
     }
-
-    return content
     
+    return content
 }
+
 export default NotesList
